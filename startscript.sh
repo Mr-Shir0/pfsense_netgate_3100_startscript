b3a3ad0b42f49c8ee1431e8b88a5d4ad8bfb4a47
A guy with a mission <46573822+Mr-Shir0@users.noreply.github.com>
2021-02-19 11:44:42 +0100

    implements grep current version and decision

--- startscript.sh
+++ startscript.sh
@@ -9,13 +9,20 @@
 # Description:		Script execute on startup
 ### END INIT INFO
 
-#Where to place: "/usr/local/etc/rc.d/startscript.sh"
+# Where to place: "/usr/local/etc/rc.d/startscript.sh"
 
-#Turn off the blue flashing LED
+# Turn off the blue flashing LED
 sleep 30
 
-# Version <= 2.4.5
-gpioctl 2 duty 0
+# Get current pfSense Version
+ver = "pkg search pfSense | grep Meta | grep -v builder | cut -f 1 -d ' ' | cut -f 2 -d '-' | tr -d '.'"
 
-# Version >= 2.5.0 || Version >= 21.02
-/usr/sbin/gpioctl -f /dev/gpioc2 2 duty 0
+if [ "$ver" -ge "2102" ] || [ "$ver" -ge "250" ] then
+  # Version >= 2.5.0 || Version >= 21.02
+  /usr/sbin/gpioctl -f /dev/gpioc2 2 duty 0
+
+else
+  # Version <= 2.4.5
+  gpioctl 2 duty 0
+
+fi
